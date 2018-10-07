3477d44f248d003a731799ba6f36708dc6d6b0b7
Hemmadi <Rajesh.Hemmadi@standardbank.co.za>
2018-10-08 00:21:47 +0200

    Authentication Done

---
+++ src/app/config/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCZ33opSUQie8zwvivIC7z8SLpYX2y2DBw",
+    authDomain: "myneweventmanagement.firebaseapp.com",
+    databaseURL: "https://myneweventmanagement.firebaseio.com",
+    projectId: "myneweventmanagement",
+    storageBucket: "myneweventmanagement.appspot.com",
+    messagingSenderId: "43403241510"
+
+};
