4fa175a818200d1c083cb16dadf50a9669820138
Hemmadi <Rajesh.Hemmadi@standardbank.co.za>
2018-10-08 00:25:30 +0200

    fixed untracked files

--- src/app/config/credentials.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCZ33opSUQie8zwvivIC7z8SLpYX2y2DBw",
-    authDomain: "myneweventmanagement.firebaseapp.com",
-    databaseURL: "https://myneweventmanagement.firebaseio.com",
-    projectId: "myneweventmanagement",
-    storageBucket: "myneweventmanagement.appspot.com",
-    messagingSenderId: "43403241510"
-
-};
